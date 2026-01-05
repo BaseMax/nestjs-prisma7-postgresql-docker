ab6f9a6d8d2c51009f09008c81ec0af1ab502182
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2026-01-05 16:27:07 +0330

    up

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,17 @@
 import { PrismaPostgresAdapterConfig, PrismaPostgresAdapter } from '@prisma/adapter-ppg'
 
-import { PrismaClient, Prisma } from './generated/client.js';
+// import { PrismaClient } from './generated/client.js';
 
-const connectionString = `${process.env.DATABASE_URL}`
-const config: PrismaPostgresAdapterConfig = {
-    connectionString: connectionString,
-};
-const adapter = new PrismaPostgresAdapter(config);
-const options: Prisma.PrismaClientOptions = {
-    adapter: adapter,
-    errorFormat: 'pretty',
-};
-const prisma = new PrismaClient(options);
+// const connectionString = `${process.env.DATABASE_URL}`
+// const config: PrismaPostgresAdapterConfig = {
+//     connectionString: connectionString,
+// };
+// const adapter = new PrismaPostgresAdapter(config);
+// const options: Prisma.PrismaClientOptions = {
+//     adapter: adapter,
+//     errorFormat: 'pretty',
+// };
+// const prisma: PrismaClient = new PrismaClient(options);
 
 export async function main() {
     // await prisma.user.create({ data: u })
