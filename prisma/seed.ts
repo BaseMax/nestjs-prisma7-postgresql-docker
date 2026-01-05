f0de4fbc4aa2441a6f3b1285c6bd8c65e3d581d2
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2026-01-05 14:04:11 +0330

    update prisma: https://github.com/prisma/prisma-examples/blob/latest/orm/nextjs/prisma.config.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient, Prisma } from './generated/client';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter })
+
+export async function main() {
+    // await prisma.user.create({ data: u })
+}
+
+main()
